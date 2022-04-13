0233a2e5d5118c1dc693b28d1a179ae093255f07
Dmitry Byk <dima-pinsk@mail.ru>
2022-04-13 09:26:19 +0300

    Update first_tests.py

--- tests/first_tests.py
+++ tests/first_tests.py
@@ -5,14 +5,14 @@ class TestCalc:
     def setup(self):
         self.calc = Calculator
 
-    def test_multiply_calculate_multiply(self):
+    def test_multiply_calculate_correctly(self):
         assert self.calc.multiply(self, 2, 2) == 4
 
-    def test_multiply_calculate_division(self):
+    def test_division_calculate_correctly(self):
         assert self.calc.division(self, 2, 2) == 1
 
-    def test_multiply_calculate_subtraction(self):
+    def test_subtraction_calculate_correctly(self):
         assert self.calc.subtraction(self, 2, 2) == 0
 
-    def test_multiply_calculate_adding(self):
+    def test_adding_calculate_correctly(self):
         assert self.calc.multiply(self, 2, 2) == 4

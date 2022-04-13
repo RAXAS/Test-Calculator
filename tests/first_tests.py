f01795d8f6d5c9fb0a0b74292259c666336fd635
Dmitry Byk <dima-pinsk@mail.ru>
2022-04-13 11:53:46 +0300

    Update first_tests.py

--- tests/first_tests.py
+++ tests/first_tests.py
@@ -15,4 +15,4 @@ class TestCalc:
         assert self.calc.subtraction(self, 2, 2) == 0
 
     def test_adding_calculate_correctly(self):
-        assert self.calc.multiply(self, 2, 2) == 4
+        assert self.calc.adding(self, 2, 2) == 4
